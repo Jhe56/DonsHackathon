f1e737850c17100b8b884942881add89aa6d52c3
AllenHo2 <aho22@dons.usfca.edu>
2025-04-05 14:55:07 -0700

    Button works to login

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ in "__main__":
     app.run(debug=True)
