51e16556c6cccf9a0f9f4306e948af49556b8a76
AllenHo2 <aho22@dons.usfca.edu>
2025-04-05 09:18:03 -0700

    Made table with some random examples data in it

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #Imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,8 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
-
+    return render_template('index.html')
 
 
 if __name__ in "__main__":
