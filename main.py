da47a0f0ff1ff1d9afe774183cc53de9f48dade6
JoanneHe <jhe56@dons.usfca.edu>
2025-04-05 17:03:41 -0700

    Main.py with comments

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return ("Message:", "Hello World")
+
+#so fast api is able to operate as a regular .py file, the main thing is that rather than just that
+#it can also return information as json lines, which we can then use to sort of add paths to images into our data banks
